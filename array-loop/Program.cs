6e25fa8502589e3f827f009c39eb07f6e531b15b
tsuyoshi.hara <alra9si916323@gmail.com>
2023-08-16 19:21:05 +0900

    array-loop

---
+++ array-loop/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+//Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+//Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+//Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+//fraudulentOrderIDs[0] = "F000";
+//Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+//Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+//string[] names = { "Rowena", "Robin", "Bao" };
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+//int[] inventory = { 200, 450, 700, 175, 250 };
+//int sum = 0;
+//int bin = 0;
+//foreach (int items in inventory)
+//{
+//    sum += items;
+//    bin++;
+//    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+//}
+//Console.WriteLine($"We have {sum} items in inventory");
+
+string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (var item in fraudulentOrderIDs)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}
